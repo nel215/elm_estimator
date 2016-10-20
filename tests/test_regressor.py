3f761a05d3f09d811096d2262d29411b7171a171
nel215 <otomo.yuhei@gmail.com>
2016-10-20 16:44:25 +0900

    Fix boston test

--- tests/test_regressor.py
+++ tests/test_regressor.py
@@ -2,7 +2,7 @@
 import numpy as np
 from sklearn.datasets import load_boston
 from sklearn.model_selection import cross_val_score, ShuffleSplit
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from elm_estimator import ELMRegressor
 
 
@@ -13,10 +13,10 @@ def test_hidden_types():
 def test_boston():
     np.random.seed(0)
     data, target = load_boston(True)
-    scaler = StandardScaler()
+    scaler = MinMaxScaler()
     data = scaler.fit_transform(data)
 
-    cv = ShuffleSplit(n_splits=10, test_size=0.5)
-    regressor = ELMRegressor(3000)
+    cv = ShuffleSplit(n_splits=20, test_size=0.05)
+    regressor = ELMRegressor(100)
     score = cross_val_score(regressor, data, target, cv=cv, n_jobs=-1)
-    assert score.mean() > 0.75
+    assert score.mean() > 0.80

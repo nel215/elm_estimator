e38193b4d16062e76fbb84cbf9abf1dcd2063ab8
nel215 <otomo.yuhei@gmail.com>
2016-10-20 16:39:35 +0900

    Add gaussain activation function

--- elm_estimator/function.py
+++ elm_estimator/function.py
@@ -1,7 +1,12 @@
 # coding:utf-8
 import numpy as np
+from scipy.spatial.distance import cdist
 
 
 def sigmoid(X, a, b):
     t = np.dot(a, X.T).T + b
     return 1.0/(1.0 + np.exp(-t))
+
+
+def gaussian(X, a, b):
+    return np.exp(-b * cdist(X, a))
--- tests/test_function.py
+++ tests/test_function.py
@@ -10,3 +10,12 @@ def test_sigmoid():
     b = np.random.randn(100)
     h = sigmoid(X, a, b)
     assert (10, 100) == h.shape
+
+
+def test_gaussian():
+    np.random.seed(0)
+    X = np.random.randn(10, 5)
+    a = np.random.randn(100, 5)
+    b = np.random.randn(100)
+    h = gaussian(X, a, b)
+    assert (10, 100) == h.shape
